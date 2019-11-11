19afe8bba479a618f9c59dfe89cc94b2640db337
Lothar Rubusch <l.rubusch@gmx.ch>
2019-11-11 22:31:20 +0100

    build.sh: initial setup

---
+++ docker__buildroot/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+export DEFCONFIG=sama5d2_xplained_graphics_defconfig
+cd ~/buildroot-at91
+###
+## don't use external repo
+#make $DEFCONFIG
+## or use external repo
+BR2_EXTERNAL=../buildroot-external-microchip/ make $DEFCONFIG
+###
+make -j8
+## not possible to mount the folder directly as 'output' inside the container
+## buildroot's make then fails right in the beginning with:
+##
+##   *** Error during update of the configuration.
+##
+
+## obtain build artifacts
+sudo chown $(whoami):$(whoami) -R ~/output
+cp -arfL ~/buildroot/output/* ~/output/
