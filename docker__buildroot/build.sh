450bb6c3e808b396911d61749c5ac6fe1ccdde1b
Lothar Rubusch <l.rubusch@gmx.ch>
2020-12-05 18:56:06 +0100

    Dockerfile: update for buildroot, using a trailing copy step

--- docker__buildroot/build.sh
+++ docker__buildroot/build.sh
@@ -1,19 +1,30 @@
 #!/bin/bash -e
+export USER="$(whoami)"
+export MY_HOME="/home/${USER}"
 export DEFCONFIG=sama5d3_xplained_graphics_defconfig
-cd ~/buildroot-at91
+
+cd ${MY_HOME}/buildroot-at91
+
 ###
 ## don't use external repo
 #make $DEFCONFIG
+
 ## or use external repo
-BR2_EXTERNAL=../buildroot-external-microchip/ make $DEFCONFIG
+BR2_EXTERNAL=../buildroot-external-microchip/ make defconfig $DEFCONFIG
+
 ###
 make -j8
-## not possible to mount the folder directly as 'output' inside the container
-## buildroot's make then fails right in the beginning with:
+
+## obtain build artifacts with a separate copy step
+##
+## not possible to mount the folder directly as 'output' inside the
+## container buildroot's make then fails right in the beginning with:
 ##
 ##   *** Error during update of the configuration.
 ##
-
-## obtain build artifacts
+## This happence since, the c function in kbuild is not able to
+## deal with mounted devices / cross device links (in other containers
+## I implemented a patch for that), now copy the build artifacts after
+## the build in a separate step
 sudo chown $(whoami):$(whoami) -R ~/output
 cp -arfL ~/buildroot-at91/output/* ~/output/
